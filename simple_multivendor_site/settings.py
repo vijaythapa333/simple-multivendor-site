3d19c8aad4ba4865fdeba5ba1dd3e4ed62065a0a
Vijay Thapa <vijaythapa333@gmail.com>
2021-06-07 20:58:39 +0545

    Static Files added

--- simple_multivendor_site/settings.py
+++ simple_multivendor_site/settings.py
@@ -108,6 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
