cd977300dcbdb768ab6abeba21e04fa7160ef90a
Vijay Thapa <vijaythapa333@gmail.com>
2021-06-11 17:44:13 +0545

    Update Cart Quantity

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .cart import Cart
 
 # Create your views here.
 def cart_detail(request):
+    cart = Cart(request)
+    remove_from_cart = request.GET.get('remove_from_cart', '')
+    change_quantity = request.GET.get('change_quantity', '')
+    quantity = request.GET.get('quantity', 0)
+
+    if remove_from_cart:
+        cart.remove(remove_from_cart)
+        return redirect('cart:cart')
+    
+    if change_quantity:
+        cart.add(change_quantity, quantity, True)
+        return redirect('cart:cart')
+        
     return render(request, 'cart/cart.html')
